fe0e12e999c1b427a1f0f0bdace7bfd81f207048
andrii248 <krupaandrii@gmail.com>
2025-09-04 15:05:03 +0200

    App header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AppHeader from './components/AppHeader/AppHeader'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CatalogPage from './pages/CatalogPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <AppHeader/>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="campers" element={<CatalogPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
+    </>
   )
 }
 
